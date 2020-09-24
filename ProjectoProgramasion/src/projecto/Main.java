a2cfe492335b6d4d1b2841b233cfa3931c57668d
izotzcristobal <izotzcristobal@localhost>
2020-09-24 15:53:00 +0200

    Primer commit

---
+++ ProjectoProgramasion/src/projecto/Main.java
@@ -0,0 +1,10 @@
+package projecto;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Egunon bizkaia, arriba españita");
+	}
+
+}
