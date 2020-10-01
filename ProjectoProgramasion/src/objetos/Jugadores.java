671bb0b3bdb5d4feb0ab3fc871597866919be6b1
izotzcristobal <izotzcristobal@localhost>
2020-10-01 16:41:07 +0200

    Empezando a crear clases/Objetos

---
+++ ProjectoProgramasion/src/objetos/Jugadores.java
@@ -0,0 +1,26 @@
+package objetos;
+
+public class Jugadores {
+	
+	String nombre;
+	String apellido;
+	int edad;
+	String nacionalidad;
+	int id_equipo;
+	
+	public Jugadores() {
+		
+	}
+	
+	public Jugadores(String nombre,String apellido,int edad, String nacionalidad, int id_equipo) {
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.edad=edad;
+		this.nacionalidad=nacionalidad;
+		this.id_equipo=id_equipo;
+	}
+	
+	
+	
+
+}
--- ProjectoProgramasion/src/projecto/Main.java
+++ ProjectoProgramasion/src/projecto/Main.java
@@ -1,4 +1,5 @@
 package projecto;
+
 public class Main {
 
 	public static void main(String[] args) {
