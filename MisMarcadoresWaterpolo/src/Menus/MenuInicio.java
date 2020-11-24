d4540df0d3bb4077fae0631c52e92fff2d7f4e9e
koldoo02 <koldo.moya@opendeusto.es>
2020-11-24 18:31:34 +0100

    Menu Inicio

--- MisMarcadoresWaterpolo/src/Menus/MenuInicio.java
+++ MisMarcadoresWaterpolo/src/Menus/MenuInicio.java
@@ -1,7 +1,57 @@
 package Menus;
 
+import java.awt.EventQueue;
+import java.awt.Font;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class MenuInicio extends JFrame { 
-	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuAdmin window = new MenuAdmin();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MenuInicio() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		setSize(411, 550);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("MENU INICIO");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(85, 25, 220, 30);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel ID = new JLabel("ID:");
+		ID.setBounds(10, 11, 46, 14);
+		getContentPane().add(ID);
+		
+		JLabel usuario = new JLabel("Usuario:");
+		usuario.setBounds(10, 24, 54, 14);
+		getContentPane().add(usuario);
+	}
 }
